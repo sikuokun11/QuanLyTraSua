cbf693ac13eba02c6b3fac0ebc8858c482543196
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2019-11-30 23:52:37 +0700

    Update category

--- ServerSideApp/app/src/main/java/com/example/serversideapp/ViewHolder/MenuViewHolder.java
+++ ServerSideApp/app/src/main/java/com/example/serversideapp/ViewHolder/MenuViewHolder.java
@@ -1,31 +1,26 @@
 package com.example.serversideapp.ViewHolder;
 
-import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.serversideapp.Common.Common;
 import com.example.serversideapp.Interface.ItemClickListener;
 import com.example.serversideapp.R;
 
-public class MenuViewHolder extends RecyclerView.ViewHolder implements
-        View.OnClickListener,
-        View.OnCreateContextMenuListener {
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     public TextView txtMenuName;
     public ImageView imageView;
 
     private ItemClickListener itemClickListener;
 
-    public MenuViewHolder(View itemView) {
+    public MenuViewHolder(View itemView){
         super(itemView);
 
         txtMenuName = itemView.findViewById(R.id.menu_name);
         imageView = itemView.findViewById(R.id.menu_image);
 
-        itemView.setOnCreateContextMenuListener(this);
         itemView.setOnClickListener(this);
 
     }
@@ -35,18 +30,11 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements
     }
 
     @Override
-    public void onClick(View view) {
-        itemClickListener.onClick(view, getAdapterPosition(), false);
+    public void onClick(View view){
+        itemClickListener.onClick(view,getAdapterPosition(),false);
     }
+}
 
-    @Override
-    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
-        contextMenu.setHeaderTitle("Select The Action");
-
-        contextMenu.add(0, 0, getAdapterPosition(), Common.UPDATE);
-        contextMenu.add(0, 1, getAdapterPosition(), Common.DELETE);
-
-    }
 }
 
 
